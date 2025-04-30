6afb84cc0872a385ff04a0d618b917fcb2533e59
bekhruzdee <qutbidinovbehruz003@gmail.com>
2025-04-30 14:56:49 +0500

    update items.service

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,34 +1,43 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateItemRequest, GetItemRequest, Item, Items } from './items';
 import { randomUUID } from 'crypto';
-import { Subject } from 'rxjs';
+import { Observable, ReplaySubject } from 'rxjs';
 
 @Injectable()
 export class ItemsService {
   private readonly items: Item[] = [];
-  private readonly itemsSubject = new Subject<Items>();
+  private readonly itemsSubject = new ReplaySubject<Items>(1);
 
-  createItem(request: CreateItemRequest) {
-    const item: Item = {
+  constructor() {
+    this.emitItems();
+  }
+
+  createItem(request: CreateItemRequest): Item {
+    const newItem: Item = {
       id: randomUUID(),
       ...request,
     };
-    this.items.push(item);
-    this.itemsSubject.next({
-      items: this.items,
-    });
-    return item;
+
+    this.items.push(newItem);
+    this.emitItems();
+    return newItem;
   }
 
-  getItem(request: GetItemRequest) {
-    const item = this.items.find((item) => item.id === request.id);
-    if (!item) {
-      throw new NotFoundException(`Item with ID ${request.id} not found`);
+  getItem(request: GetItemRequest): Item {
+    const foundItem = this.items.find((item) => item.id === request.id);
+
+    if (!foundItem) {
+      throw new NotFoundException(`Item with ID "${request.id}" not found`);
     }
-    return item;
+
+    return foundItem;
   }
 
-  streamItems() {
+  streamItems(): Observable<Items> {
     return this.itemsSubject.asObservable();
+  }
+
+  private emitItems(): void {
+    this.itemsSubject.next({ items: [...this.items] });
   }
 }
